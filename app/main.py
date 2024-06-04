d87c1b7c11003998d5a563f49a9b656f42b53eff
Sudonuma <tasnim.hd@gmail.com>
2024-06-04 21:06:31 +0200

    Deploy St app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,77 @@
 import streamlit as st
 import pickle5 as pickle
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+def get_scaled_values(input_data):
+    data = get_clean_data()
+
+    X = data.drop(['diagnosis'], axis=1)
+
+    scaled_dict = {}
+
+    for key, value in input_data.items():
+        max_val = X[key].max()
+        min_val = X[key].min()
+        scaled_value = (value - min_val) / (max_val - min_val)
+        scaled_dict[key] = scaled_value
+
+    return scaled_dict
+
+def get_radar_chart(input_data):
+
+    input_data = get_scaled_values(input_data)
+
+    categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                'Smoothness', 'Compactness', 
+                'Concavity', 'Concave Points',
+                'Symmetry', 'Fractal Dimension']
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatterpolar(
+        r=[
+            input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+            input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+            input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+            input_data['fractal_dimension_mean']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Mean Value'
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=[
+            input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
+            input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
+            input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Standard Error'
+    ))
+    fig.add_trace(go.Scatterpolar(
+        r=[
+            input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+            input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+            input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+            input_data['fractal_dimension_worst']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Worst Value'
+    ))
+
+    fig.update_layout(
+    polar=dict(
+        radialaxis=dict(
+        visible=True,
+        range=[0, 1]
+        )),
+    showlegend=True
+    )    
+    return fig
 
 
 def get_clean_data():
@@ -62,6 +133,29 @@ def add_sidebar():
     return input_dict
 
 
+def add_predictions(input_data):
+    model = pickle.load(open("model/model.pkl", "rb"))
+    scaler = pickle.load(open("model/scaler.pkl", "rb"))
+
+    input_array = np.array(list(input_data.values())).reshape(1, -1)
+
+    input_array_scaled = scaler.transform(input_array)
+    
+
+    prediction = model.predict(input_array_scaled)
+
+    st.subheader("Cell cluster prediction")
+    st.write("The cell cluster is:")
+    if prediction[0] == 0:
+        st.write("<span class='diagnosis benign' style='padding: 0.2em 0.5em; color:white; background-color: green; border-radius: 0.5em;'>Benign</span>", unsafe_allow_html=True)
+
+    else:
+        st.write("<span class='diagnosis Malicious' style='padding: 0.2em 0.5em; color:white; background-color: Red; border-radius: 0.5em;'>Malicious</span>", unsafe_allow_html=True)
+    st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
+    st.write("Probability of being Malicious: ", model.predict_proba(input_array_scaled)[0][1])
+    st.write("This App can assist a medical professional in making diagnosis but should not be used as a substitute for a professional diagnosis")
+
+
 
 
 def main():
@@ -83,12 +177,11 @@ def main():
     coll1, coll2 = st.columns([4,1])
 
     with coll1:
-        st.write("this is column 1")
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
 
     with coll2:
-        st.write("this is column 2")
-
-
+        add_predictions(input_data)
 
 
 
